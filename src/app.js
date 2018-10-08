05ec8badc39176905194272398c993a9c15559d4
Sanuja91 <sanujac@live.com>
2018-10-08 15:27:20 +0530

    Added handlePick function downstream

--- src/app.js
+++ src/app.js
@@ -1,13 +1,40 @@
 class IndecisionApp extends React.Component {
+  constructor(props) {
+    super(props)
+    this.handleDeleteOptions = this.handleDeleteOptions.bind(this)
+    this.handlePick = this.handlePick.bind(this)
+    this.state = {
+      options: ["Thing one", "Thing two", "Thing four"]
+    }
+  }
+
+  handleDeleteOptions() {
+    this.setState(() => {
+      return {
+        options: []
+      }
+    })
+  }
+
+  handlePick() {
+    const randomNum = Math.floor(Math.random() * this.state.options.length)
+    const option = this.state.options[randomNum]
+    alert(option)
+  }
+
   render() {
     const title = "Indecision"
     const subtitle = "Put your life in the hands of a computer"
-    const options = ["Thing one", "Thing two", "Thing four"]
     return (
       <div>
         <Header title={title} subtitle={subtitle} />
-        <Action />
-        <Options options={options} />
+        <Action
+          hasOptions={this.state.options.length > 0}
+          handlePick={this.handlePick} />
+        <Options
+          options={this.state.options}
+          // Can pass method onto sub-component
+          handleDeleteOptions={this.handleDeleteOptions} />
         <AddOption />
       </div>
     )
@@ -26,35 +53,23 @@ class Header extends React.Component {
 }
 
 class Action extends React.Component {
-  handlePick() {
-    alert("handlePick")
-  }
   render() {
     return (
       <div>
-        <button onClick={this.handlePick}>What should I do?</button>
+        <button onClick={this.props.handlePick} disabled={!this.props.hasOptions}>
+          What should I do?
+        </button>
       </div>
     )
   }
 }
 
 class Options extends React.Component {
-  constructor(props) {
-    // Have to do this to override any behavior
-    super(props)
-      // bind() is used to pass on values, such as this to the next function / object
-      // allowing them to refer to the same object / component
-    this.handleRemoveAll = this.handleRemoveAll.bind(this)
-  }
-
-  handleRemoveAll() {
-    console.log(this.props.options)
-  }
-
   render() {
     return (
+      // Calls handleDeleteOptions that was passed down from IndecisionApp
       <div>
-        <button onClick={this.handleRemoveAll.bind(this)}>Remove All</button>
+        <button onClick={this.props.handleDeleteOptions}>Remove All</button>
         {this.props.options.map(option => {
           return <Option key={option} optionText={option} />
         })}
