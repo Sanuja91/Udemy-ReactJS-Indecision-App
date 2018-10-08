2b27fb8395090c0d82174d1852cdfb394208ce52
Sanuja91 <sanujac@live.com>
2018-10-08 15:57:52 +0530

    Passing data upstream to classes

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -14,10 +14,12 @@ var IndecisionApp = function (_React$Component) {
   function IndecisionApp(props) {
     _classCallCheck(this, IndecisionApp);
 
+    // Bind anything that you want to pass downstream
     var _this = _possibleConstructorReturn(this, (IndecisionApp.__proto__ || Object.getPrototypeOf(IndecisionApp)).call(this, props));
 
     _this.handleDeleteOptions = _this.handleDeleteOptions.bind(_this);
     _this.handlePick = _this.handlePick.bind(_this);
+    _this.handleAddOption = _this.handleAddOption.bind(_this);
     _this.state = {
       options: ["Thing one", "Thing two", "Thing four"]
     };
@@ -41,6 +43,17 @@ var IndecisionApp = function (_React$Component) {
       alert(option);
     }
   }, {
+    key: "handleAddOption",
+    value: function handleAddOption(option) {
+      if (!option) return 'Enter valid value to add item';else if (this.state.options.indexOf(option) > -1) return 'This option already exists';
+
+      this.setState(function (prevState) {
+        return {
+          options: prevState.options.concat(option)
+        };
+      });
+    }
+  }, {
     key: "render",
     value: function render() {
       var title = "Indecision";
@@ -56,7 +69,8 @@ var IndecisionApp = function (_React$Component) {
           options: this.state.options
           // Can pass method onto sub-component
           , handleDeleteOptions: this.handleDeleteOptions }),
-        React.createElement(AddOption, null)
+        React.createElement(AddOption, {
+          handleAddOption: this.handleAddOption })
       );
     }
   }]);
@@ -187,18 +201,27 @@ var Option = function (_React$Component5) {
 var AddOption = function (_React$Component6) {
   _inherits(AddOption, _React$Component6);
 
-  function AddOption() {
+  function AddOption(props) {
     _classCallCheck(this, AddOption);
 
-    return _possibleConstructorReturn(this, (AddOption.__proto__ || Object.getPrototypeOf(AddOption)).apply(this, arguments));
+    var _this6 = _possibleConstructorReturn(this, (AddOption.__proto__ || Object.getPrototypeOf(AddOption)).call(this, props));
+
+    _this6.handleAddOption = _this6.handleAddOption.bind(_this6);
+    _this6.state = {
+      error: undefined
+    };
+    return _this6;
   }
 
   _createClass(AddOption, [{
     key: "handleAddOption",
     value: function handleAddOption(e) {
       e.preventDefault();
-      var option = e.target.elements.option.value;
-      if (option) alert(option);
+      var option = e.target.elements.option.value.trim();
+      var error = this.props.handleAddOption(option);
+      this.setState(function () {
+        return { error: error };
+      });
     }
   }, {
     key: "render",
@@ -206,6 +229,11 @@ var AddOption = function (_React$Component6) {
       return React.createElement(
         "div",
         null,
+        this.state.error && React.createElement(
+          "p",
+          null,
+          this.state.error
+        ),
         React.createElement(
           "form",
           { onSubmit: this.handleAddOption },
