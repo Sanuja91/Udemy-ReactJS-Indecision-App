31f1d707702d767a6fe8d41d8babbbe2981edd80
sanuja91 <sanuja.c@foodrunner.asia>
2018-10-09 18:03:31 +0530

    Switched from live-server to webpack dev-server

--- webpack.config.js
+++ webpack.config.js
@@ -22,5 +22,8 @@ module.exports = {
             exclude:/node_modules/
         }]
     },
-    devtool:'cheap-module-eval-source-map'
+    devtool:'cheap-module-eval-source-map',
+    devServer:{
+        contentBase:path.join(currentLoc, 'public')
+    }
 }
