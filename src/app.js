73162ebcf5ad1d54036ab95b882f16488d5ae270
sanuja91 <sanuja.c@foodrunner.asia>
2018-10-09 18:21:20 +0530

    Remove test code in app.js

--- src/app.js
+++ src/app.js
@@ -2,31 +2,4 @@ import React from "react"
 import ReactDOM from "react-dom"
 import IndecisionApp from "./components/IndecisionApp"
 
-class OldSyntax {
-  constructor() {
-    this.name = "Mike"
-    this.getGreeting = this.getGreeting.bind(this)
- }
- getGreeting(){
-     return `Hi. My name is ${this.name}`
- }
-}
-
-const oldSyntax = new OldSyntax()
-console.log(oldSyntax.getGreeting())
-
-// Can't do this
-
-// ----------
-
-class NewSyntax {
-  name = "Jen"
-  getGreeting =()=>{
-    return `Hi. My name is ${this.name}` 
-  }
-}
-
-const newSyntax = new NewSyntax()
-console.log(newSyntax.getGreeting())
-
 ReactDOM.render(<IndecisionApp />, document.getElementById("app"))
