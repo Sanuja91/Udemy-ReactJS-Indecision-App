0a8ad8f2423628cc834288383d93ee2e36cc93e2
sanuja91 <sanuja.c@foodrunner.asia>
2018-10-09 22:43:06 +0530

    Added SCSS sytles and respective loaders to webpack

--- webpack.config.js
+++ webpack.config.js
@@ -1,29 +1,40 @@
-const path = require('path')
+const path = require("path")
 
 // Contains path to currnet location
 const currentLoc = __dirname
 
 // Where the application kick off - entry?
 module.exports = {
-    entry: './src/app.js',
-    // Where to output the final bundle file?
-    output: {
-        path: path.join(currentLoc, 'public'),
-        filename: 'bundle.js'
-    },
+  entry: "./src/app.js",
+  // Where to output the final bundle file?
+  output: {
+    path: path.join(currentLoc, "public"),
+    filename: "bundle.js"
+  },
 
-    // Adds the loaders such as babel to convert ES6 to ES5
-    module: {
-        rules: [{
-            loader: 'babel-loader',
-
-            // Runs through files ending with js
-            test: /\.js$/,
-            exclude:/node_modules/
-        }]
-    },
-    devtool:'cheap-module-eval-source-map',
-    devServer:{
-        contentBase:path.join(currentLoc, 'public')
-    }
+  // Adds the loaders such as babel to convert ES6 to ES5
+  module: {
+    rules: [
+      {
+        loader: "babel-loader",
+        // Runs through files ending with js
+        test: /\.js$/,
+        exclude: /node_modules/
+      },
+      {
+        // Runs through files ending with css
+        test: /\.scss$/,
+        // Use allows us to use an array of loaders
+        use: [
+            'style-loader',
+            'css-loader',
+            'sass-loader'
+        ]
+      }
+    ]
+  },
+  devtool: "cheap-module-eval-source-map",
+  devServer: {
+    contentBase: path.join(currentLoc, "public")
+  }
 }
